6b7a01aeffe85239389a1b40f01d6ffb18ef539a
Fadhil Radhian <fadhilradhian@gmail.com>
2021-06-10 19:32:57 +0700

    init

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  env : {
+    API_TOKEN : process.env.BUTTER_API_TOKEN,
+    BASE_URL : process.env.BASE_URL
+  }
+}
---
+++ pages/items/[id].js
@@ -0,0 +1,41 @@
+import styles from '../../styles/ProductPage.module.css'
+import {getAllProductIds, getProductData} from '../../lib/products'
+
+export default function ProductPage({productData}) {
+  return (
+    <div className={styles.container}>
+       <div className={styles.img}>
+           <img  
+              src={productData.image} 
+              alt={`Preview of ${productData.name}`}
+              style={{ "max-width": "100%", "max-height": "20rem"}}
+          />  
+        </div>
+        <div className={styles.productDetails}>
+        <p className={styles.title}>
+          {productData.name}
+        </p>
+        <p className={styles.description}>{productData.description}</p>
+        <p>{productData.price}</p>
+       </div>
+    </div>
+  )
+}
+
+export async function getStaticPaths() {
+    const paths = await getAllProductIds();
+    return     {
+      paths, 
+      fallback: false
+    }
+}
+
+export async function getStaticProps({params}) {
+    const productData = await getProductData(params.id)
+    console.log(productData);
+    return {
+      props: {
+        productData
+      }
+    }
+}
